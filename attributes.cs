38d49f2ee72c83908ac0ce59eddedd56c3f93f22
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2021-12-22 17:26:24 +0530

    Initial Commit

---
+++ attributes.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Attributes and we can create custom attributes
+ * Read more for usage
+ * */
+#define DEBUG
+using System;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace SanjjeevWorkouts
+{
+    public class Test
+    {   
+        // Conditional only runs when specific preprocessor exist
+        // Obsolete gives error when place above function defintion and displays message
+
+        [Conditional("DEBUG")]
+
+        [Obsolete("Try to use new Old Method, Use new Method", true)]
+
+        public void getMessageOld()
+        {
+            Console.WriteLine("This is test message");
+        }
+
+        public void getMessageNew()
+        {
+            Console.WriteLine("This is new test message");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+            obj.getMessageNew();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ reflection concepts.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Reflections are used to get type of information when runtime
+ * and mostly preferable for debug information
+ * */
+
+using System;
+using System.Linq;
+
+/* Attribute definitions */
+
+public class HelpAttribute : System.Attribute
+{
+    public readonly string Url;
+
+    public string Topic
+    {
+        get { return topic; }
+        set { topic = value;  }
+    }
+
+    private string topic;
+    public HelpAttribute(string url)
+    {
+        this.Url = url;
+    }
+}
+
+/* Attribute definitions end */
+
+[HelpAttribute("Information on the class TestClass")]
+class TestClass
+{
+    public string text;
+}
+
+namespace SanjjeevWorkouts
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Reflection.MemberInfo Info = typeof(TestClass);
+            object[] attributes = Info.GetCustomAttributes(true);
+
+            for (int i=0; i<attributes.Length; i++)
+            {
+                Console.WriteLine(attributes[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
